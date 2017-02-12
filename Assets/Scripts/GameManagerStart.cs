60b87c23530d1f80e0f5c6160eab75964b5f6d9e
Enrico Speranza <enrico.speranza@gmail.com>
2017-02-12 19:38:01 +0100

    Serializable Object now usable witk VRTK

---
+++ Assets/Scripts/GameManagerStart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerStart : MonoBehaviour {
+
+    public GameObject Prefab;
+
+    bool quitting;
+
+    // Use this for initialization
+    void Start () {
+        
+        //Make game working in background
+
+        //Load all serializable objects
+        SerializableManager.LoadAll();
+
+        //Create object
+        //GameObject newObject = SerializableManager.PrefabInstantiate(Prefab);
+        //newObject.transform.position = new Vector3(9.908f, 0.645f, 8.2f);
+
+    }
+
+    // On quiting Application save all
+    void OnApplicationQuit()
+    {
+        quitting = true;
+        //Save all serializable objects	
+        SerializableManager.SaveAll();
+    }
+    void OnDestroy()
+    {
+        if (quitting)
+        {
+            //application is quitting; don't spawn more stuff
+        }
+        else
+        {
+            //application is running, proceed normally
+        }
+    }
+}
---
+++ Assets/Scripts/PrefabSaveSerializable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(SerializableComponent))]
+[Save(true,true,true)]
+public class PrefabSaveSerializable : MonoBehaviour {
+    //Save rot, trans, scale 
+}
