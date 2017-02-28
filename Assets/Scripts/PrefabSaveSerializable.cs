5aedcc5a65a5533a272dabbd37de4018b2931f07
Vytek <enrico.speranza@gmail.com>
2017-02-28 15:15:26 +0100

    Destroy object if y negative

--- Assets/Scripts/PrefabSaveSerializable.cs
+++ Assets/Scripts/PrefabSaveSerializable.cs
@@ -7,4 +7,11 @@ using UnityEngine;
 [Save(true,true,true)]
 public class PrefabSaveSerializable : MonoBehaviour {
     //Save rot, trans, scale 
+
+	void Update() {
+		//If under the ground destroy
+		if (transform.position.y < -2) {
+			Destroy(gameObject);
+		}
+	}
 }
